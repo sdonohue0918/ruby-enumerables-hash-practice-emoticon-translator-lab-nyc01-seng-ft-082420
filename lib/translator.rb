e3fbcb0eec054c6d4c9e35f1a1ed62429aa6ac09
Sean Donohue <sdonohue0918@gmail.com>
2020-08-19 15:35:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ end
 
 def get_japanese_emoticon(emoticons_file, english_emoticon)
   lexicon = load_library(emoticons_file)
-  binding.pry
+  
   sorry_message = "Sorry, that emoticon was not found"
   ret_string=""
 
